9d61bb50758a0bef8cf2be9ee75320924a4d8f8b
parkavi817 <148421660+parkavi817@users.noreply.github.com>
2025-11-30 13:28:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -44,13 +44,14 @@ mongoose
 // Middleware
 app.use(cors({
   origin: [
-    "http://localhost:5173",         
-    "https://your-frontend.netlify.app"  // you will update this later
+    "http://localhost:5173",
+    process.env.FRONTEND_URL   // Netlify frontend URL will come from Render env vars
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
+
 
 app.use(express.json());
 
@@ -83,3 +84,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
